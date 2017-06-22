7121b0eb345254861e8990d1cfe49637f3a3098c
jakchen <jakchen9935208@qq.com>
2017-06-22 22:29:42 +0800

    2017-6-22

---
+++ demo/HTML5拖放/app.js
@@ -0,0 +1,30 @@
+var box1Div,msgDiv,img1;
+window.onload=function (){
+  box1Div = document.getElementById("box1");
+  msgDiv = document.getElementById("msg");
+  img1 = document.getElementById("img1");
+  // box1Div.ondragenter = function(e){
+  //   showObj(e);
+  // }
+  box1Div.ondragover =function (e){
+    e.preventDefault();
+  }
+  img1.ondragstart = function (e){
+    e.dataTransfer.setData("imgId","img1");
+  }
+  box1Div.ondrop = function (e){
+    showObj(e.dataTransfer);
+    e.preventDefault();
+    //
+    var img = document.getElementById(e.dataTransfer.getData("imgId"));
+    box1Div.appendChild(img);
+
+  }
+}
+function showObj(obj){
+  var s="";
+  for(var k in obj){
+    s+=k+":"+obj[k]+"<br/>"
+  }
+  msgDiv.innerHTML=s;
+}
